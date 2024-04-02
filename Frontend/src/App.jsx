40d086f5c38baeaefc988dfb0ede77c905e0b404
Pabasaraa <pabasara.stpd@gmail.com>
2024-04-02 22:19:03 +0530

    Initial commit with frontend boilerplate code

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <div></div>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
